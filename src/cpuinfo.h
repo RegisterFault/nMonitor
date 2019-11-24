5a52c60aa380fc1141f0ce423abe1814ff18ce5e
Travis James <registerfault@gmail.com>
2019-11-23 20:15:56 -0500

    tidy up function declaration

--- src/cpuinfo.h
+++ src/cpuinfo.h
@@ -27,7 +27,7 @@ char get_throttle_char(void);
 int get_pl1(void);
 int get_pl2(void);
 double get_pkg_joules(void);
-double get_intel_pkg_joules();
+double get_intel_pkg_joules(void);
 double get_amd_pkg_joules();
 double get_pp0_joules(void);
 double get_volt( unsigned int);
