c6e154979cde1810ce6feb8f2df79f4bf00a1506
Travis James <registerfault@gmail.com>
2019-11-25 11:18:36 -0500

    tidy up list.* to comply with linux kernel coding standards

--- src/list.c
+++ src/list.c
@@ -4,13 +4,13 @@
 
 
 
-struct node * last_elem(struct node * in)
+struct node *last_elem(struct node *in)
 {
-        for(in; in->next != NULL; in = in->next) {}
+        for( in; in->next != NULL; in = in->next) {}
         return in;
 }
 
-void add_node(struct node * in, long int a)
+void add_node(struct node *in, long int a)
 {
         in = last_elem(in);
         in->next = malloc(sizeof(struct node));
@@ -19,26 +19,26 @@ void add_node(struct node * in, long int a)
         in->next = NULL;
 }
 
-int count_elems(struct node * in)
+int count_elems(struct node *in)
 {
         int i = 0;
-        for(in; in->next != NULL; in = in->next)
+        for (in; in->next != NULL; in = in->next)
                 i++;
         return i;
 }
 
-struct node * nth_elem(struct node * in, int num)
+struct node *nth_elem(struct node *in, int num)
 {
-        if(num > (count_elems(in) - 1))
+        if (num > (count_elems(in) - 1))
                 return last_elem(in);
-        for(int i = 0; i<num; i++)
+        for (int i = 0; i<num; i++)
                 in = in->next;
         return in;
 }
 
-struct node * free_top(struct node * in)
+struct node *free_top(struct node *in)
 {
-        struct node * prev;
+        struct node *prev;
         
         prev = in;
         in = in->next;
@@ -47,12 +47,13 @@ struct node * free_top(struct node * in)
         return in;
 }
 
-struct node * free_to_nth(struct node * in, int n)
+struct node *free_to_nth(struct node *in, int n)
 {
-        struct node * prev;
-        if(n > count_elems(in))
+        struct node *prev;
+        if (n > count_elems(in))
                 return in;
-        for(int i=0; i<n; i++) {
+
+        for (int i=0; i<n; i++) {
                 prev = in;
                 in = in->next;
                 free(prev);
@@ -65,30 +66,30 @@ int get_val_y(long int max, long int lines, long int in)
         float a = in;
         float b = max;
         float c = lines;
-        return (int) (c-((a/b)*c)) -1;
+        return (int) (c - ((a / b) * c)) -1;
 }
 
-struct node * draw_graph(WINDOW * win, struct node * list, long int max)
+struct node *draw_graph(WINDOW *win, struct node *list, long int max)
 {
         int lines = 0;
         int cols = 0;
         int x = 0;
         int y;
-        struct node * cur;
+        struct node *cur;
         int size = count_elems(list);
-        getmaxyx(win,lines,cols);
+        getmaxyx(win, lines, cols);
         wclear(win);
-        box(win,0,0);
+        box(win, 0, 0);
 
-        while(size > (cols-2) && size > 0 ){
+        while (size > (cols - 2) && size > 0 ){
                 list = free_top(list);
                 size = count_elems(list);
         }
         cur = list;
         
         for(cur; cur->next != NULL; cur = cur->next){
-                for(y = get_val_y(max,lines-2,cur->foo); y<lines-2; y++)
-                        mvwprintw(win, y+1, x+1, "*");
+                for(y = get_val_y(max, lines - 2, cur->foo); y < lines - 2; y++)
+                        mvwprintw(win, y + 1, x + 1, "*");
                 x++;
         }
         
--- src/list.h
+++ src/list.h
@@ -2,17 +2,17 @@
 #define LIST_H
 
 struct node {
-        struct node * next;
+        struct node *next;
         long int foo;
 };
 
 
-struct node * last_elem(struct node *);
+struct node *last_elem(struct node *);
 void add_node(struct node *, long int );
 int count_elems(struct node *);
-struct node * nth_elem(struct node *, int);
-struct node * free_top(struct node *);
-struct node * free_to_nth(struct node *, int );
+struct node *nth_elem(struct node *, int);
+struct node *free_top(struct node *);
+struct node *free_to_nth(struct node *, int );
 int get_val_y(long int, long int, long int);
-struct node * draw_graph(WINDOW *, struct node *, long int);
+struct node *draw_graph(WINDOW *, struct node *, long int);
 #endif
