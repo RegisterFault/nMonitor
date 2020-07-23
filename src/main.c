2e86d5d18e0c9475c66a13f5d41956920c9c68f7
Travis James <registerfault@gmail.com>
2020-07-23 13:22:53 -0400

    various whitespace adjustments
    add more code clarity and be more consistent in coding style.

--- src/main.c
+++ src/main.c
@@ -17,21 +17,21 @@
 void draw_amperage(WINDOW *win, struct node *list)
 {
         mvwprintw(win, 0, 0, "%ld mW -- Battery: %d%% -- Capacity: %2.1f/%2.1f Ah",
-                        last_elem(list)->data,
-                        get_charge_pct(),
-                        get_charge_full(),
-                        get_charge_full_design());
+                  last_elem(list)->data,
+                  get_charge_pct(),
+                  get_charge_full(),
+                  get_charge_full_design());
         wrefresh(win);
         add_node(list, get_charge_wattage());
 }
 
 void draw_wattage(WINDOW *win, struct node *list)
 {
-        mvwprintw(win,0,0,"%ld mW -- Battery: %d%% -- Capacity: %d/%d Wh",
-                last_elem(list)->data,
-                get_bat_pct(),
-                get_bat_full(),
-                get_bat_design());
+        mvwprintw(win, 0, 0, "%ld mW -- Battery: %d%% -- Capacity: %d/%d Wh",
+                  last_elem(list)->data,
+                  get_bat_pct(),
+                  get_bat_full(),
+                  get_bat_design());
         wrefresh(win);
         add_node(list, get_wattage());
 }
@@ -51,7 +51,7 @@ void draw_freq(WINDOW *win, struct node **list)
         int max_boost = get_boost_freq();
 
         draw_graph(win, list, max_boost);
-        mvwprintw(win,0,0,"%ld MHz",last_elem(*list)->data);
+        mvwprintw(win, 0, 0, "%ld MHz", last_elem(*list)->data);
 
         wrefresh(win);
         add_node(*list, get_freq());
@@ -74,16 +74,16 @@ void draw_cpu(WINDOW *win)
 
         mvwprintw(win, line++, 0, "CPU INFO: %s", cpu_brand);
         mvwprintw(win, line++, 1, "%s %dC/%dT",
-                        cpu_name,
-                        get_cores(),
-                        get_threads());
+                  cpu_name,
+                  get_cores(),
+                  get_threads());
         mvwprintw(win, line++, 1, "Temp: %dC", get_temp());
         mvwprintw(win, line++, 1, "Boost: %s ", get_turbo() ? "on" : "off" );
         mvwprintw(win, line++, 1, "Gov: %s", governor);
         if (is_root() && have_msr()){ 
                 if (last_pkg_nrg != 0)
                         mvwprintw(win, line++, 1, "PKG:   %6.2f W",
-                                        (get_pkg_joules() - last_pkg_nrg) / DUR_SEC);
+                                  (get_pkg_joules() - last_pkg_nrg) / DUR_SEC);
                 else
                         mvwprintw(win, line++, 1, "PKG:   %6.2f W", 0.0);
                 last_pkg_nrg = get_pkg_joules();
@@ -108,8 +108,9 @@ void draw_mem(WINDOW *win)
         int line = 0;
         
         get_mem(&mem);
+        
         wclear(win);
-        box(win,0,0);
+        box(win, 0, 0);
         
         // /proc/cpuinfo reports in KiB, despite actually showing kB 
         mvwprintw(win, line++, 0, "MEM INFO");
@@ -119,7 +120,6 @@ void draw_mem(WINDOW *win)
         mvwprintw(win, line++, 1, "Cache: %5ld MiB", mem.cache / 1024);
         /* this appears to be inaccurate */
         mvwprintw(win, line++, 1, "Used:  %5ld MiB", (mem.total - mem.avail) / 1024);
-        
         wrefresh(win);
 }
 
@@ -135,12 +135,12 @@ int main()
 
         init_batinfo();
 
+        /* ncurses setup */
         initscr();
         curs_set(0);
 
         cpuwin = newwin(14, 20, 0, 0);
         memwin = newwin(10, 20, 14, 0);
-        
         wwin = newwin(12, 50, 0, 20);
         fwin = newwin(12, 50, 12, 20);
 
