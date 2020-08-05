9e3f3f0e8e4a61f4d68b66bbb6661a86c450bb62
Travis James <registerfault@gmail.com>
2020-08-05 17:12:13 -0400

    Restructure application to handle SIGWINCH resize events

--- src/main.c
+++ src/main.c
@@ -6,55 +6,79 @@
 #include <fcntl.h>
 #include <string.h>
 #include <math.h>
+#include <signal.h>
 #include "cpuinfo.h"
 #include "list.h"
 #include "msr.h"
 #include "draw.h"
 
-int main()
-{
+struct {
         WINDOW *wwin;
         WINDOW *fwin;
         WINDOW *cpuwin;
         WINDOW *memwin;
         WINDOW *fgwin;
-        WINDOW *msrwin;
-        struct node *wlist = init_node();
-        struct node *flist = init_node();
+        struct node *wlist;
+        struct node *flist;
 #define STATS_MODE 0
 #define GRID_MODE 1
-        int mode = STATS_MODE;
+        int mode;
+} app;
+        
 
-        init_batinfo();
-        init_powerinfo();
-
+void draw_app()
+{
         /* ncurses setup */
         initscr();
         curs_set(0);
         noecho();
         cbreak();
         nodelay(stdscr, 1);
+        
+        app.cpuwin = newwin(15, 20, 0, 0);
+        app.memwin = newwin(9, 20, 15, 0);
+        app.wwin =   newwin(12, 50, 0, 20);
+        app.fwin =   newwin(12, 50, 12, 20);
+        app.fgwin =  newwin(24,70,0,0);
+}
 
-        cpuwin = newwin(15, 20, 0, 0);
-        memwin = newwin(9, 20, 15, 0);
-        wwin = newwin(12, 50, 0, 20);
-        fwin = newwin(12, 50, 12, 20);
-        fgwin = newwin(24,70,0,0);
-        msrwin = newwin(24,70,0,0);
+void init_app()
+{
+        init_batinfo();
+        init_powerinfo();
+
+        app.wlist = init_node();
+        app.flist = init_node();
+        app.mode = STATS_MODE;
+
+        draw_app();
+}
+
+void handle_resize(int unused)
+{
+        endwin();
+        draw_app();
+}
+
+int main()
+{
+
+        init_app();
+        signal(SIGWINCH, handle_resize);
 
         while (1) {
                 if(getch() == 'f')
-                        mode = !mode;
+                        app.mode = !app.mode;
                 flushinp();
 
-                if (mode == STATS_MODE) {
-                        draw_cpu(cpuwin);
-                        draw_mem(memwin);
-                        draw_power(wwin, &wlist);
-                        draw_freq(fwin, &flist);
+                if (app.mode == STATS_MODE) {
+                        draw_cpu(app.cpuwin);
+                        draw_mem(app.memwin);
+                        draw_power(app.wwin, &app.wlist);
+                        draw_freq(app.fwin, &app.flist);
                         doupdate();
-                } else if (mode == GRID_MODE) {
-                        draw_grid(fgwin);
+                } else if (app.mode == GRID_MODE) {
+                        draw_grid(app.fgwin);
                         doupdate();
                 }
 
